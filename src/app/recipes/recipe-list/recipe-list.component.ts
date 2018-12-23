f0bf4f2cadf63e1b4718008ba75dd9b0ef0789d8
RemekGdansk <Lukasz.Klonowski@gmail.com>
2018-12-23 18:33:25 +0100

    After S03L049

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,6 +12,11 @@ export class RecipeListComponent {
       'A Test Recipe',
       'This is simply a test',
       'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe 2',
+      'This is simply a test 2',
+      'https://upload.wikimedia.org/wikipedia/commons/a/a3/07603_pierogi_zapiekane_z_kapusta_i_kminem.jpg'
     )
   ];
 
